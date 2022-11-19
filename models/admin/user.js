6052710242be909eeb22817f63c70ab320f6fa17
arunk555 <arunkurusev@gmail.com>
2022-11-19 06:28:11 +0000

    latest changes

---
+++ models/admin/user.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+const schema=new Schema({
+    id:{
+       type: Number,
+       required: true,
+       autoincrement: true,
+       unique: true,
+       createindex: true
+    },
+    name:{
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength:100,
+        default: null
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        default: null,
+        minlength: 3,
+        maxlength:150
+    },
+    password:{
+        type: String,
+        default: null,
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        default:()=>Date.now()
+    },
+    updatedAt:{
+        type: Date,
+        default:()=>Date.now()
+    }
+});
+
+module.exports=model("adminlogin", schema);
